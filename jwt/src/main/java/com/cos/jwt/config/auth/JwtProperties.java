179d0e75c1846881f57ed5d53194e9ad114dfb1e
mg <lkcbap12@gmail.com>
2023-10-10 14:59:44 +0900

    first commit

--- jwt/src/main/java/com/cos/jwt/config/auth/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.cos.jwt.config.auth;
-
-public interface JwtProperties {
-    String SECRET = "cos";
-    int EXPRIRATION = 60000 * 10;
-    String TOKEN_PREFIX = "Bearer";
-    String HEADER_STRING = "Authorization";
-}
